12a2a6515210cb075ea9a94b51901e012cf2ffad
Obaloluwa Odelana <obaloluwaode@gmail.com>
2021-12-21 00:37:00 +0100

    Inital commit

--- pedmas-calc.c
+++ pedmas-calc.c
@@ -7,6 +7,11 @@
 
 #define TOKEN_SIZE 100
 
+typedef struct Operation
+{
+
+} Operation;
+
 char* get_expression(const char **, int);
 char **tokenise_expression(char *, int *);
 double compute_expression(char *);
@@ -18,43 +23,7 @@ int main(int argc, const char *argv[])
     {
         if (equationStr != NULL && strlen(equationStr) > 0)
         {
-            int tokenCount = 0;
-            // Turn string into comprehensible array of strings
-            char **tokens = tokenise_expression(equationStr, &tokenCount);
-            if (tokens != NULL)
-            {
-                #ifndef DEBUG_MODE
-                    double sum = 0;
-                    char currentOperator = '+';
-                #endif
-                for (int i = 0; i < tokenCount; i++)
-                {
-                    char *token = tokens[i];
-
-                    #ifdef DEBUG_MODE
-                        printf("%s\n", token);
-                    #else
-                        // Even indexes contain operators
-                        if (i % 2 != 0)
-                            currentOperator = token[0];
-                        else
-                            // If operator is a not an addition sign add the negative result to sum
-                            sum += (currentOperator == '+' ? 1 : -1) * compute_expression(token);
-                    #endif
-
-                    free(token);
-                }
-                // Print expression and answer in format "3 * 2 + 3 = 9"
-                // Check if answer is integer or double
-                #ifndef DEBUG_MODE
-                    if ((int) sum == sum) // Integer
-                        printf("%s = %d\n", equationStr, (int) sum);
-                    else // Double
-                        printf("%s = %f\n", equationStr, sum);
-                #endif
-
-                free(tokens);
-            }
+            
         }
         free(equationStr);
         equationStr = get_expression(NULL, 0);
@@ -98,19 +67,6 @@ char* get_expression(const char **args, int argCount)
     return expression;
 }
 
-// Utility function to show a message and free allocated memory
-// Need to have address to pointers to free them, hence the "***"
-void free_tokens(const char *message, char ***tokens, int len)
-{
-    printf("%s\n", message);
-    for (int i = 0; i < len; i++)
-    {
-        if ((*tokens)[i] != NULL)
-            free((*tokens)[i]);
-    }
-    free(*tokens);
-}
-
 void remove_spaces(char *str)
 {
     char *duplicate = str;
@@ -124,167 +80,3 @@ void remove_spaces(char *str)
     } while((*str++ = *duplicate++));
 }
 
-char **tokenise_expression(char *eq, int *length)
-{
-    remove_spaces(eq);
-
-    int currentIndex = 0;
-
-    char **tokens = malloc(sizeof(char*) * 1);
-    tokens[0] = calloc(TOKEN_SIZE, sizeof(char));
-
-    *length = 1;
-
-    char c = eq[0], lastC = 0;
-    for (int i = 0; i < strlen(eq); i++)
-    {
-        c = eq[i];
-        int charIsSign = strchr("+-*/^.", c) != NULL;
-
-        if (charIsSign)
-        {
-            // Check if last character and current character are signs
-            if (strchr("+-*/^.", lastC) != NULL)
-            {
-                free_tokens("Invalid expression", &tokens, *length);
-                return NULL;
-            }
-
-            // Check if operator is the last character
-            if (eq[i + 1] == '\0')
-            {
-                free_tokens("Incomplete expression", &tokens, *length);
-                return NULL;
-            }
-        }
-
-        // Allow plus or minus sign at the start of the expression
-        if (i == 0 && strchr("+-", c) != NULL)
-            strncpy(tokens[currentIndex], &c, 1);
-
-        // Treat numbers with "*/^." as single quantities to enforce PEDMAS
-        else if (isdigit(c) || strchr("*/^.", c) != NULL)
-        {
-            // If character equals any of the operators
-            if (strchr("*/^.", c) != NULL)
-            {
-                // If operator is the first character in string
-                if (strlen(tokens[currentIndex]) == 0)
-                {
-                    free_tokens("Don't start an expression with an operator", &tokens, *length);
-                    return NULL;
-                }
-            }
-
-            strncat(tokens[currentIndex], &c, 1);
-        }
-
-        else if (strchr("+-", c))
-        {
-            // Put plus and minus operators on their own line
-            currentIndex++;
-
-            // Allocate space for two more rows
-            tokens = realloc(tokens, sizeof(char*) * (currentIndex + 2));
-            // Allocate memory for first row
-            tokens[currentIndex] = calloc(1, sizeof(char));
-            // Put sign in first row
-            strncpy(tokens[currentIndex], &c, 1);
-
-            // Allocate memory for second row
-            tokens[++currentIndex] = calloc(TOKEN_SIZE, sizeof(char));
-            
-            // Two new rows added
-            *length += 2;
-        }
-
-        // If character not supported
-        else if (!isspace(c))
-        {
-            free_tokens("Invalid character found in expression", &tokens, *length);
-            return NULL;
-        }
-
-        lastC = c;
-    }
-
-    return tokens;
-}
-
-void perform_operation(double operand, double *out, char operator)
-{
-    switch (operator)
-    {
-        case '+':
-            *out += operand;
-            break;
-
-        case '-':
-            *out -= operand;
-            break;
-
-        case '*':
-            *out *= operand;
-            break;
-
-        case '/':
-            *out /= operand;
-            break;
-        
-        default:
-            printf("Tokenisation error\n");
-            exit(EXIT_FAILURE);
-            break;
-    }
-}
-
-double compute_expression(char *token)
-{
-    double result = 0, lastNum = 1;
-    char c = token[0], currentSign = '+', lastSign = 0;
-    for (int i = 0, len = strlen(token); i < len; i++)
-    {
-        c = token[i];
-
-        // A number, plus or minus sign, or parenthesis
-        if (isdigit(c) || (i == 0 && strchr("+-", c) != NULL))
-        {
-            double num = 0;
-            if (isdigit(c) || strchr("+-", c) != NULL)
-            {
-                // Pointer to where strtod stopped parsing 
-                char *end;
-                num = strtod(token + i, &end);
-
-                // Increment index to skip the parsed numbers
-                i += (end - (token + i)) - 1;
-            }
-            
-            // If current and next operator is not an exponent sign
-            if (token[i + 1] != '^' && currentSign != '^')
-                perform_operation(num, &result, currentSign);
-            else if (currentSign == '^')
-            {
-                double exponentResult = pow(lastNum, num);
-                if (lastSign == '^')
-                {
-                    printf("Unsupported use of exponent sign\n");
-                    return result;
-                }
-                perform_operation(exponentResult, &result, lastSign);
-            }
-            
-            // Save current number
-            lastNum = num;
-        }
-
-        // Other signs
-        else if (strchr("+-/*^", c) != NULL)
-        {
-            lastSign = currentSign;
-            currentSign = c;
-        }
-    }
-
-    return result;
-}
